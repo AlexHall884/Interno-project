3e24d41224b212c512edce34ce4d72824f782da6
Alex_Kleshin <alexhall884@gmail.com>
2024-01-25 20:32:29 +0300

    added final work

---
+++ interno/src/blocks/FooterBlockComp.vue
@@ -0,0 +1,207 @@
+<template>
+    <div>
+        <!-- Добавил ранее -->
+        <footer class="footer center">
+            <div class="footer__link">
+                <div class="link__heading">
+                    <a href="index.html" class="heading__logo">
+                        <svg class="heading__logo-img" xmlns="http://www.w3.org/2000/svg" width="34" height="34"
+                            viewBox="0 0 34 34" fill="none">
+                            <path
+                                d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
+                                fill="#CDA274" />
+                        </svg>
+                        <span class="header__logo-text">Interno</span>
+                    </a>
+                </div>
+                <p class="link__text">It is a long established fact that a
+                    reader will be distracted lookings.</p>
+                <div class="media">
+                    <a href="#" class="media__link"><img src="../assets/twitter.svg" alt="logo"></a>
+                    <a href="#" class="media__link"><img src="../assets/in.svg" alt="logo"></a>
+                </div>
+            </div>
+            <div class="footer__pages">
+                <h3 class="pages__tittle">Pages</h3>
+                <div class="pages__menubar">
+                    <router-link class="pages__menubar-item" to="/home">Home</router-link>
+                    <router-link class="pages__menubar-item" to="/project">Projects</router-link>
+                    <router-link class="pages__menubar-item" to="/blog">Blog</router-link>
+                </div>
+            </div>
+            <div class="footer__contact">
+                <h3 class="contact__tittle">Contact</h3>
+                <p class="contact__item">55 East Birchwood Ave. Brooklyn, New
+                    York 11201</p>
+                <p class="contact__item">contact@interno.com</p>
+                <p class="contact__item">(123) 456 - 7890</p>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterBlockComp',
+
+    data() {
+        return {
+
+        }
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "/src/stylesheets/_vars.scss";
+@import "/src/stylesheets/style.scss";
+
+.footer {
+    margin-bottom: 134px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 101px;
+
+    .footer__link {
+        display: flex;
+        flex-direction: column;
+
+        .heading__logo {
+            color: $tittleColor;
+            font-family: DM Serif Display;
+            font-size: 40px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 125%;
+            margin-bottom: 18px;
+            display: flex;
+            align-items: center;
+            gap: 8px;
+            transition: all 0.3s;
+
+            &:hover {
+                scale: 1.1;
+                color: $colorSiteSelect;
+            }
+
+            &:hover path {
+                fill: $tittleColor;
+            }
+        }
+
+        .link__text {
+            color: $textColor;
+            font-family: Jost;
+            font-size: 22px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 150%;
+            /* 33px */
+            letter-spacing: 0.22px;
+            width: 360px;
+            margin-bottom: 31px;
+        }
+
+        .media {
+            display: flex;
+            flex-direction: row;
+            gap: 54px;
+
+            &__link:hover {
+                scale: 1.5;
+            }
+        }
+    }
+
+    .footer__pages {
+        display: flex;
+        flex-direction: column;
+        gap: 9px;
+
+        .pages__tittle {
+            color: $tittleColor;
+            font-family: DM Serif Display;
+            font-size: 25px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 125%;
+            /* 31.25px */
+        }
+
+        .pages__menubar {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .pages__menubar-item {
+            color: $textColor;
+            font-family: Jost;
+            font-size: 22px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 300%;
+            /* 66px */
+            letter-spacing: 0.22px;
+            transition: all 0.3s;
+
+            &:hover {
+                scale: 1.1;
+                color: $colorSiteSelect;
+            }
+        }
+    }
+
+    .footer__contact {
+        display: flex;
+        flex-direction: column;
+
+        .contact__tittle {
+            color: $tittleColor;
+            font-family: DM Serif Display;
+            font-size: 25px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 125%;
+            /* 31.25px */
+            margin-bottom: 26px;
+        }
+
+        .contact__item {
+            color: $textColor;
+            font-family: Jost;
+            font-size: 22px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 150%;
+            /* 33px */
+            letter-spacing: 0.22px;
+            width: 258px;
+            padding-bottom: 48px;
+        }
+    }
+}
+
+//tablet styles
+@media (max-width: 1024px) {
+    .footer {
+        flex-direction: column;
+        gap: 52px;
+        align-items: center;
+
+        &__link {
+            align-items: center;
+        }
+    }
+}
+
+//mobiles styles
+</style>
---
+++ interno/src/pages/test.vue
@@ -0,0 +1,124 @@
+<template>
+    <div>
+        <div ref="swiper" class="swiper-container">
+            <div class="swiper-wrapper">
+                <div v-for="(image, index) in images" :key="index" class="swiper-slide">
+                    <img :src="image.src" :alt="image.alt" @click="openZoomedImage(image.src)" />
+                </div>
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
+        <!-- <button class="swiper-button-next" @click="nextSlide">Next</button>
+        <button class="swiper-button-prev" @click="prevSlide">Prev</button> -->
+
+    </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+import 'swiper/scss'
+import 'swiper/swiper-bundle.css';
+
+export default {
+    name: 'TestComp',
+
+    data() {
+        return {
+            images: [
+                { src: require("../assets/project_photo9.png"), alt: "photo 1" },
+                { src: require("../assets/content_photo8.png"), alt: "photo 2" },
+                { src: require("../assets/content_photo7.png"), alt: "photo 3" }
+            ],
+            swiperInstance: null
+        };
+    },
+    mounted() {
+        this.swiperInstance = new Swiper(this.$refs.swiper, {
+            loop: true,
+            pagination: {
+                el: ".swiper-pagination",
+                type: "bullets",
+                clickable: true
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
+            }
+        });
+    },
+    methods: {
+        nextSlide() {
+            this.swiperInstance.slideNext();
+        },
+        prevSlide() {
+            this.swiperInstance.slidePrev();
+        },
+        openZoomedImage(imageSrc) {
+            // Replace with logic to open a modal or trigger zoom functionality for the clicked image
+            console.log("Zoom in on image: " + imageSrc);
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.swiper-container {
+    width: 75%;
+    height: 200px;
+    margin: 20px auto;
+    position: relative;
+}
+
+.swiper-slide img {
+    width: 75%;
+    height: 75%;
+    object-fit: cover;
+    cursor: pointer;
+}
+
+.swiper-pagination {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 10px;
+    width: 100%;
+}
+
+.swiper-pagination-bullet {
+    width: 8px;
+    height: 8px;
+    background: red;
+    color: blue;
+    opacity: 0.5;
+    margin: 0 5px;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+.swiper-pagination-bullet-active {
+    background: #fff;
+    opacity: 1;
+}
+
+// .swiper-pagination {
+//     position: absolute;
+//     bottom: 10px;
+//     text-align: center;
+//     width: 100%;
+// }
+
+// .swiper-pagination-bullet {
+//     width: 8px;
+//     height: 8px;
+//     display: inline-block;
+//     border-radius: 50%;
+//     background: #000;
+//     margin: 0 5px;
+//     cursor: pointer;
+
+// }
+
+// .swiper-pagination-bullet-active {
+//     background: #fff;
+// }
+</style>
